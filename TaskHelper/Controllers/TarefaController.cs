4b04c617f896c63a3d581017456c0646e14d53ef
Henrique Walace <henriquewalace@hotmail.com>
2024-06-03 19:45:58 -0300

    feat: Cria Controller para Tarefas com função CriarTarefa

---
+++ TaskHelper/Controllers/TarefaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskHelper.Context;
+using TaskHelper.Models;
+
+namespace TaskHelper.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TarefaController : Controller
+    {
+        private readonly OrganizadorContext _context;
+
+        public TarefaController(OrganizadorContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CriarTarefa(Tarefa tarefa)
+        {
+            _context.Add(tarefa);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
