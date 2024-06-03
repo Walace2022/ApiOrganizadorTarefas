93c425da40820af55551c336adcd1c04470e5bbf
Henrique Walace <henriquewalace@hotmail.com>
2024-06-03 18:42:10 -0300

    feat: Cria modelo de Tarefa

---
+++ TaskHelper/Models/Tarefa.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskHelper.Models;
+public class Tarefa
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Descricao { get; set; }
+    public DateTime data { get; set; }
+    public int MyProperty { get; set; }
+    public EnumStatusTarefa Status { get; set; }
+}
