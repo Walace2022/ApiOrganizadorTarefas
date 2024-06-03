088fda16ba5e4fb26b05fdf8a062ba05d3c0e16f
Henrique Walace <henriquewalace@hotmail.com>
2024-06-03 19:02:48 -0300

    feat: Configura conexão com banco de dados

--- TaskHelper/Program.cs
+++ TaskHelper/Program.cs
@@ -1,6 +1,15 @@
+
+
+using Microsoft.EntityFrameworkCore;
+using TaskHelper.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<OrganizadorContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao"))
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
