ef158aa909453c4bb6832fd93676139e0ee7fe74
Henrique Walace <henriquewalace@hotmail.com>
2024-06-03 18:56:19 -0300

    feat: Cria contexto Organizador

---
+++ TaskHelper/Context/OrganizadorContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskHelper.Models;
+
+namespace TaskHelper.Context;
+public class OrganizadorContext : DbContext
+{
+    public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options) { }
+
+    public DbSet<Tarefa> Tarefas { get; set; }
+}
